db7f29b0e33f09a28f657a21f39badb47351b226
ruthansah <19594471+ruthansah@users.noreply.github.com>
2017-10-25 11:22:21 -0400

    star rating template and css

---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'pm-star',
+    templateUrl:'./star.component.html',
+    styleUrls:['./star.component.css']
+})
+export class StarComponent{
+    
+    rating: number = 4;
+    starWidth: number;
+
+}
