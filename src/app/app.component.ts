401f9998cafd606dcaf2f7336c72c56ac59986a0
ruthansah <19594471+ruthansah@users.noreply.github.com>
2017-12-13 20:06:23 -0500

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
+import { ProductService } from './products/product.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  template: `
+            <div>
+              <nav class='navbar navbar-default'>
+                <div class='container-fluid'>
+                  <a class='navbar-brand'>{{pageTitle}}</a>
+                  <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['/welcome']">Home</a></li>
+                    <li><a [routerLink]="['/products']">Product List</a></li>
+                  </ul>
+                </div>
+              </nav>
+              <div class='container'>
+                <router-outlet></router-outlet>
+              </div>            
+            </div> 
+  `,
+  providers: [ProductService]
 })
 export class AppComponent {
-      title: string = 'cooking with Ruth';
+      pageTitle: string = 'Cooking with Ruth';
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { WelcomeComponent } from './home/welcome.component';
 import { ProductService } from './products/product.service';
 import { StarComponent } from './shared/star.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
@@ -5,20 +6,33 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent  } from './products/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent ,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo:'welcome', pathMatch:'full'},
+      {path: '**', redirectTo:'welcome', pathMatch:'full'}
+    ], { useHash: false})
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
