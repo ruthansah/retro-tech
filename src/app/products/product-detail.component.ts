d95fae6069323882312f9d1667b5f28223611ed5
ruthansah <19594471+ruthansah@users.noreply.github.com>
2017-12-13 20:05:21 -0500

    Add files via upload

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,31 @@
+import { IProduct } from './product';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+
+  constructor(private _route: ActivatedRoute) { }
+
+  ngOnInit() {
+    let id= +this._route.snapshot.paramMap.get('id');/// "+" converts the string to a numeric id 
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+   }
+  }
+
+}
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,8 +1,8 @@
 import { IProduct } from './product';
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls:['./product-list.component.css']
 })
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin:number = 2;
     showImage: boolean = false;
+    errorMessage: string;
     
     _listFilter: string;
 
@@ -27,42 +28,10 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts : IProduct[];
-    products: IProduct[] = [
-            {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+    products: IProduct[];
 
-    constructor(){
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private _productService: ProductService){
+
     }
 
     performFilter(filterBy: string): IProduct[]{
@@ -76,6 +45,12 @@ export class ProductListComponent implements OnInit{
     }
 
      ngOnInit():void{
-        console.log("In OnInit");
-    }
+        this._productService.getProducts()
+        .subscribe(products => {
+            this.products = products;
+            this.filteredProducts = this.products;
+        },
+            error => this.errorMessage = <any>error);
+}
+    
 }
